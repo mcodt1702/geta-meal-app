39a8173156808b284ad8a9febc2c2978abd60316
mcodt1702 <marco@mcnyc.net>
2020-09-04 21:53:20 -0400

    data

--- src/App.js
+++ src/App.js
@@ -11,44 +11,14 @@ import RegisterRestaurant from "./components/Registration/RegisterRest";
 import TopPage from "./components/TopPage/TopPage";
 import LoginPage from "./routes/LoginPage";
 import PrivateRoute from "./utilities/PrivateRoute";
-import Restaurant from "./components/Restaurant/Restaurant";
+import Restaurants from "./components/Restaurant/Restaurant";
 import Dashboard from "./components/Dashboard/Dashboard";
 import AddMenuItems from "./components/Dashboard/AddMenuItems";
+import Restaurant from "./data/restUsers";
 
 class App extends Component {
   state = {
-    restaurant: [
-      {
-        id: 1,
-        name: "Joe's Pizza",
-        address: "2020 marine rd",
-        zip: 12099,
-        phone: "(918)555-5511",
-        username: "joe@pizza.com",
-        password: "joe33",
-        type: "Italian",
-      },
-      {
-        id: 2,
-        name: "Larrys Pizza",
-        address: "1809 Seaside rd",
-        zip: 12099,
-        phone: "(918)555-8900",
-        username: "larry@larrpizza.com",
-        password: "lar11",
-        type: "American",
-      },
-      {
-        id: 3,
-        name: "Baja Tacos",
-        address: "1809 Oak ave",
-        zip: 12095,
-        phone: "(918)333-5500",
-        username: "bajatacos@tacos.com",
-        password: "tac44",
-        type: "Mexican",
-      },
-    ],
+    restaurant: [{ Restaurant }],
     menu: [
       {
         id: 1,
