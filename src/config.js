f9e2d62b19d59884111a8430a3ac95c9732b88e0
mcodt1702 <marco@mcnyc.net>
2020-10-05 16:45:11 -0400

    vercel2

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: `https://whispering-eyrie-41220.herokuapp.com/`,
+  API_ENDPOINT: `https://secret-gorge-68198.herokuapp.com/`,
   TOKEN_KEY: "auth-token",
 
   API_KEY: process.env.REACT_APP_API_KEY,
