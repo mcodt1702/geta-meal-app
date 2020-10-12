56a8737913510853a21a522cda4b45993040a678
mcodt1702 <marco@mcnyc.net>
2020-10-12 10:37:17 -0400

    testing

--- test/App.test.js
+++ test/App.test.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import ReacDom from "reac-dom";
 import App from "../src/App";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+
+  ReacDom.render(<App />, div);
+
+  ReactDOM.unmountComponentAtNode(div);
 });
